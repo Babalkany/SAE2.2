c7f9a0297d4b608d5261a8058f151f6ea49d61a1
Babalkany <98455276+Babalkany@users.noreply.github.com>
2022-11-22 17:49:09 +0100

    ajout gestion qt-astropy
    ajout de l'affichage de l'image dans Pyqt5

--- main.py
+++ main.py
@@ -3,47 +3,76 @@
 # software qt qui combine des images en .fits   #
 # @author: Bastien BRUNEL & Tom LECLERCQ        #
 # 22/11/2022                                    #
-# V1.0 init affichage images                    #
+# V1.1 ajout gestion qt-astropy                 #
 #################################################
 
-#import 
+#import qt
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QVBoxLayout
+from PyQt5.QtGui import QImage 
+
+#import astropy
 import numpy as np
 import matplotlib.pyplot as plt
-
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 
-def chargerAllImages(listImage):
-    imageConcat = [fits.getdata(image) for image in listImage]
-    
-    # Version détaillé
-    finalImage = np.zeros(shape=imageConcat[0].shape)
+#var globales test
+test = [['C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0001.fits', 
+'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0002.fits', 
+'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0003.fits', 
+'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0004.fits', 
+'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0005.fits']]
 
-    for image in imageConcat:
-        finalImage += image
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
         
-    # Pour les fast learner
-    #finalImage = np.sum(imageConcat, axis=0)
+        # attribut liste des chemins d'images
+        self.liste = []
+        self.setWindowTitle('SAE 2.2')
+
+        #chargement des fichiers
+        self.openFileNamesDialog()
+
+        self.chargerAllImages(self.liste[0])
+        print(self.liste)
+
+        #affichage
+        self.show()
+
+    def openFileNamesDialog(self):
+        """
+        méthode qui charge plusieurs fichiers et les ajoutes directement dans self.liste
+        """
+        #                          widget parent, label explication, chemin ou ouvrir la boite de dialogue, type de fichier a ouvrir
+        files, _ = QFileDialog.getOpenFileNames(self,"Select one or more files to open", "","Images (*.fits)")
+        if files:
+            self.liste.append(files)
     
-    image_hist = plt.hist(finalImage.flatten(), bins='auto')
-    plt.show()
-    
-    plt.imshow(finalImage, cmap='gray', vmin=2E3, vmax=3E3)
-    plt.colorbar()
-    plt.show()
+    def chargerAllImages(self, listImage):
+        imageConcat = [fits.getdata(image) for image in listImage]
+        
+        # Version détaillé
+        finalImage = np.zeros(shape=imageConcat[0].shape)  # type: ignore
+
+        for image in imageConcat:
+            finalImage += image  # type: ignore
+            
+        # Pour les fast learner
+        #finalImage = np.sum(imageConcat, axis=0)
+        
+        image_hist = plt.hist(finalImage.flatten(), bins='auto')
+        plt.show()
+        
+        plt.imshow(finalImage, cmap='gray', vmin=2E3, vmax=3E3)
+        plt.colorbar()
+        plt.show()
 
 
-testt = ['C:/Users/SDO/Desktop/BUT1/Semestre_3/SAE3.02/fits_tests/M13_blue//M13_blue_0001.fits',
-        'C:/Users/SDO/Desktop/BUT1/Semestre_3/SAE3.02/fits_tests/M13_blue//M13_blue_0002.fits',
-        'C:/Users/SDO/Desktop/BUT1/Semestre_3/SAE3.02/fits_tests/M13_blue/M13_blue_0003.fits',
-        'C:/Users/SDO/Desktop/BUT1/Semestre_3/SAE3.02/fits_tests/M13_blue/M13_blue_0004.fits',
-        'C:/Users/SDO/Desktop/BUT1/Semestre_3/SAE3.02/fits_tests/M13_blue/M13_blue_0005.fits']
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
 
-testb = ['C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0001.fits', 
-        'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0002.fits', 
-        'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0003.fits', 
-        'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0004.fits', 
-        'C:/Users/Vidox/OneDrive/Documents/Cours/2/S3/SAE/C2/fits_tests/M13_blue/M13_blue_0005.fits'
-        ]
 
-chargerAllImages(testb)
