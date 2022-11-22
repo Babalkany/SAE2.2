d56401d68affa04da5f1eb537c35ad491a0073ea
Babalkany <98455276+Babalkany@users.noreply.github.com>
2022-11-22 17:51:49 +0100

    Ajout médiane & moyenne /pixel + choix dans qt
    Méthode de calcul de la médiane et de la moyenne
    Ajout de la possibilité de choisir de calculer soit la médiane, soit la moyenne par pixel et de mettre a jour la fenêtre qt

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # software qt qui combine des images en .fits   #
 # @author: Bastien BRUNEL & Tom LECLERCQ        #
 # 22/11/2022                                    #
-# V1.1 ajout gestion qt-astropy                 #
+# V1.2 choix median / moyenne dans qt           #
 #################################################
 
 #import qt
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QVBoxLayout, QPushButton
 from PyQt5.QtGui import QImage 
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 #import astropy
 import numpy as np
@@ -31,48 +32,102 @@ class App(QWidget):
         # attribut liste des chemins d'images
         self.liste = []
         self.setWindowTitle('SAE 2.2')
+        self.figure = plt.figure()
 
-        #chargement des fichiers
+        # jointure plt/qt
+        self.canvas = FigureCanvas(self.figure)
+
+        # création d'un petit layout rapide
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+        # création boutons 
+        self.btnAvg = QPushButton("Moyenne")
+        self.btnMed = QPushButton("Médiane")
+
+        # création connexions bouton/méthode
+        self.btnMed.clicked.connect(self.btnMedClicked)
+        self.btnAvg.clicked.connect(self.btnAvgClicked)
+
+        # layout 2 le retour
+        layout.addWidget(self.btnAvg)
+        layout.addWidget(self.btnMed)
+
+        # appel fichieres
         self.openFileNamesDialog()
-
-        self.chargerAllImages(self.liste[0])
-        print(self.liste)
 
         #affichage
         self.show()
+
+    def btnAvgClicked(self):
+        """
+        méthode qui calcule l'image composée des pixels moyens de tous les fichiers présent dans self.liste
+        """
+        self.chargerAllImages(self.liste[0], option = 1) 
+
+    def btnMedClicked(self):
+        """
+        méthode qui calcule l'image composée des pixels médians de tous les fichiers présent dans self.liste
+        """
+        self.chargerAllImages(self.liste[0], option = 2)
 
     def openFileNamesDialog(self):
         """
         méthode qui charge plusieurs fichiers et les ajoutes directement dans self.liste
         """
         #                          widget parent, label explication, chemin ou ouvrir la boite de dialogue, type de fichier a ouvrir
-        files, _ = QFileDialog.getOpenFileNames(self,"Select one or more files to open", "","Images (*.fits)")
+        files, _ = QFileDialog.getOpenFileNames(self,"Select one or more files to open", "","Images (*.fits / *.fit)")
         if files:
             self.liste.append(files)
     
-    def chargerAllImages(self, listImage):
+    def chargerAllImages(self, listImage, option=1):
+        self.figure.clear()
         imageConcat = [fits.getdata(image) for image in listImage]
-        
-        # Version détaillé
-        finalImage = np.zeros(shape=imageConcat[0].shape)  # type: ignore
+        copieImage = imageConcat[0]
 
-        for image in imageConcat:
-            finalImage += image  # type: ignore
+
+            # print(image_concat[i])
+        if option == 1:
+            for allPixel in imageConcat[1:]:
+                for lignePixel in range(len(allPixel)):
+                    for pixel in range(len(allPixel[lignePixel])):
+                        copieImage[lignePixel][pixel] += allPixel[lignePixel][pixel]
+                    
+
+            for lignePixel in range(len(copieImage)):  # type: ignore
+                for pixel in range(len(copieImage[lignePixel])):  # type: ignore
+                    copieImage[lignePixel][pixel] = copieImage[lignePixel][pixel] / len(imageConcat)  # type: ignore
+
+        #médiane
+        elif option == 2:
+            listAllPixel = []
+            for allPixel in imageConcat[0:]:
+                for lignePixel in range(len(allPixel)):  # type: ignore
+                    for pixel in range(len(allPixel[lignePixel])):  # type: ignore
+                        listAllPixel.append(allPixel[lignePixel][pixel])  # type: ignore
+        
+         
+            listAllPixelTriee = sorted(listAllPixel) 
             
-        # Pour les fast learner
-        #finalImage = np.sum(imageConcat, axis=0)
-        
-        image_hist = plt.hist(finalImage.flatten(), bins='auto')
-        plt.show()
-        
-        plt.imshow(finalImage, cmap='gray', vmin=2E3, vmax=3E3)
+            milieu = listAllPixelTriee[(len(listAllPixelTriee)-1)//2]
+            
+            for lignePixel in range(len(copieImage)):  # type: ignore
+                for pixel in range(len(copieImage[lignePixel])):  # type: ignore
+                    copieImage[lignePixel][pixel] = milieu            # type: ignore
+        print(copieImage)
+
+        plt.imshow(copieImage, cmap='gray')  # type: ignore
         plt.colorbar()
-        plt.show()
+        # plt.show()
+        self.canvas.draw()
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
+    ex.show()
     sys.exit(app.exec_())
 
 
